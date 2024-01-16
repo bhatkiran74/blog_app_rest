36c7f09b0fd422a7ca3fce91e33fa528a8e6fa8f
kiran <bhatkiran74@gmail.com>
2024-01-17 01:02:55 +0530

    created comment repository implementation

---
+++ src/main/java/com/kiran/blog_app_rest/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.kiran.blog_app_rest.repository;
+
+import com.kiran.blog_app_rest.entity.Comment;
+import com.kiran.blog_app_rest.entity.Post;
+
+public interface CommentRepository extends BaseRepository<Comment,Long>{
+}
