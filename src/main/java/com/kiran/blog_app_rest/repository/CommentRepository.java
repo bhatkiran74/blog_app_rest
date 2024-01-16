f8064d2a50fa9dd1f99e6d8aad321a077569aea8
kiran <bhatkiran74@gmail.com>
2024-01-17 01:03:53 +0530

    removed sonarqube

--- src/main/java/com/kiran/blog_app_rest/repository/CommentRepository.java
+++ src/main/java/com/kiran/blog_app_rest/repository/CommentRepository.java
@@ -1,7 +1,6 @@
 package com.kiran.blog_app_rest.repository;
 
 import com.kiran.blog_app_rest.entity.Comment;
-import com.kiran.blog_app_rest.entity.Post;
 
 public interface CommentRepository extends BaseRepository<Comment,Long>{
 }
